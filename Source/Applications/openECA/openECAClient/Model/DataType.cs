96a3f58cf50fff522e31fa60cd42fda1ac52654c
StephenCWills <swills@gridprotectionalliance.org>
2016-05-25 15:33:20 -0400

    Removed unused usings from UDT data models.

--- Source/Applications/openECA/openECAClient/Model/DataType.cs
+++ Source/Applications/openECA/openECAClient/Model/DataType.cs
@@ -21,12 +21,6 @@
 //
 //******************************************************************************************************
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace openECAClient.Model
 {
     public class DataType
