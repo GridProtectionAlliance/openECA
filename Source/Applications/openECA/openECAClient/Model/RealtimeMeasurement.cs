7570a99a0a3c53760489a3c72cd05db7eddfd016
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2016-09-27 16:46:51 -0400

    Added Grafana interface support on ports 6362 and 6462 for stats and data respectively.

---
+++ Source/Applications/openECA/openECAClient/Model/RealtimeMeasurement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openECAClient.Model
+{
+    public class Measurement
+    {
+        public Double Timestamp { get; set; }
+        public Double Value { get; set; }
+        public Guid ID { get; set; }
+    }
+}
