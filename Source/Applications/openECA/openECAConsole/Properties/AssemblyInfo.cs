6f5b3d12c3754c0553326f534c1b2a1ca7a4714d
gsfbuildbot <support@gridprotectionalliance.org>
2017-01-18 00:20:31 -0500

    openECA: Version change for build v0.8.17.0-master.

--- Source/Applications/openECA/openECAConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/openECA/openECAConsole/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("0.8.16.0")]
+[assembly: AssemblyVersion("0.8.17.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
