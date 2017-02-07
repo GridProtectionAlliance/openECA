fe1c180750ad84726492f9878038a8d7c1d1ed64
gsfbuildbot <support@gridprotectionalliance.org>
2017-02-07 00:21:27 -0500

    openECA: Version change for build v0.8.32.0-master.

--- Source/Templates/FSharp/AlgorithmTemplate/AssemblyInfo.fs
+++ Source/Templates/FSharp/AlgorithmTemplate/AssemblyInfo.fs
@@ -33,9 +33,9 @@ open System.Runtime.InteropServices
 // 
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [<assembly: AssemblyVersion("0.8.31.0")>]
-[<assembly: AssemblyVersion("0.8.31.0")>]
-[<assembly: AssemblyFileVersion("0.8.31.0")>]
+// [<assembly: AssemblyVersion("0.8.32.0")>]
+[<assembly: AssemblyVersion("0.8.32.0")>]
+[<assembly: AssemblyFileVersion("0.8.32.0")>]
 
 do
     ()
