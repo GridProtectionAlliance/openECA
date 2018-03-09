9608565ff88bd1fccbdbb08911a77f8add988ef2
gsfbuildbot <support@gridprotectionalliance.org>
2018-03-09 00:37:14 -0500

    openECA: Version change for build v1.1.20.0-master.

--- Source/Templates/VisualBasic/AlgorithmTemplateLibrary/My Project/AssemblyInfo.vb
+++ Source/Templates/VisualBasic/AlgorithmTemplateLibrary/My Project/AssemblyInfo.vb
@@ -29,7 +29,7 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.1.19.0")> 
+' <Assembly: AssemblyVersion("1.1.20.0")> 
 
-<Assembly: AssemblyVersion("1.1.19.0")>
-<Assembly: AssemblyFileVersion("1.1.19.0")>
+<Assembly: AssemblyVersion("1.1.20.0")>
+<Assembly: AssemblyFileVersion("1.1.20.0")>
