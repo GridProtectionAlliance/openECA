b5f14226dd6dd1362a043ec986466a2e5b7864ba
gsfbuildbot <support@gridprotectionalliance.org>
2019-03-16 20:46:43 -0400

    openECA: Version change for build v1.2.143.0-master.

--- Source/Analytics/LocalizedVARController/Source/LocalVoltageController/C#code/VoltController4/Properties/AssemblyInfo.cs
+++ Source/Analytics/LocalizedVARController/Source/LocalVoltageController/C#code/VoltController4/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.142.0")]
-[assembly: AssemblyVersion("1.2.142.0")]
-[assembly: AssemblyFileVersion("1.2.142.0")]
+// [assembly: AssemblyVersion("1.2.143.0")]
+[assembly: AssemblyVersion("1.2.143.0")]
+[assembly: AssemblyFileVersion("1.2.143.0")]
