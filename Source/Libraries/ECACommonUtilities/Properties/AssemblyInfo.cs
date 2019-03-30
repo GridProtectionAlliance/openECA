6a29f1df6822325b4322f6beaa5b26e87cfea208
gsfbuildbot <support@gridprotectionalliance.org>
2019-03-30 00:35:26 -0400

    openECA: Version change for build v1.2.148.0-master.

--- Source/Libraries/ECACommonUtilities/Properties/AssemblyInfo.cs
+++ Source/Libraries/ECACommonUtilities/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.147.0")]
-[assembly: AssemblyVersion("1.2.147.0")]
-[assembly: AssemblyFileVersion("1.2.147.0")]
+// [assembly: AssemblyVersion("1.2.148.0")]
+[assembly: AssemblyVersion("1.2.148.0")]
+[assembly: AssemblyFileVersion("1.2.148.0")]
