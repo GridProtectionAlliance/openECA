2856914f77687fd2cf2e9d3ce6285ea8e36a2e86
gsfbuildbot <support@gridprotectionalliance.org>
2023-10-18 00:59:48 -0400

    Updated GSF dependencies.

--- Source/Data/SQL Server/InitialDataSet.sql
+++ Source/Data/SQL Server/InitialDataSet.sql
@@ -758,6 +758,3 @@ INSERT INTO AlarmState (State, Color) VALUES ('Acknowledged', 'rosybrown')
 GO 
 INSERT INTO DataOperation(Description, AssemblyName, TypeName, MethodName, Arguments, LoadOrder, Enabled) VALUES('ECA Startup Operations', 'ECAServerFramework.dll', 'ECAServerFramework.ECAStartupOperations', 'PerformECAStartupOperations', '', 3, 1)
 GO
- 
-INSERT INTO DataOperation(Description, AssemblyName, TypeName, MethodName, Arguments, LoadOrder, Enabled) VALUES('ECA Startup Operations', 'ECAServerFramework.dll', 'ECAServerFramework.ECAStartupOperations', 'PerformECAStartupOperations', '', 3, 1)
-GO
